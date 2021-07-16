f7cdd1853825e06a4aada9b20e14470a84d1f322
kk_boomer <fking.dmitry@gmail.com>
2021-07-16 18:29:40 +0200

    git init

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'view/timer_page.dart';
+
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(109, 234, 255, 1),
+        accentColor: Color.fromRGBO(72, 74, 126, 1),
+        brightness: Brightness.dark,
+      ),
+      title: 'Flutter Timer',
+      home: const TimerPage(),
+    );
+  }
+}
---
+++ lib/view/timer_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timer_with_bloc/bloc/timer_bloc.dart';
+import 'package:timer_with_bloc/view/timer.dart';
+
+import 'background.dart';
+import 'ticker.dart';
+
+
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => TimerBloc(ticker: Ticker()),
+      child: const TimerView(),
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Timer')),
+      body: Stack(
+        children: [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(child: TimerText()),
+              ),
+              ActionsTimer(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesStr =
+        ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondsStr = (duration % 60).floor().toString().padLeft(2, '0');
+    return Text(
+      '$minutesStr:$secondsStr',
+      style: Theme.of(context).textTheme.headline1,
+    );
+  }
+}
